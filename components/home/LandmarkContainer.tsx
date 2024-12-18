6d3136967075abfae01bd30488792cc43daed8a1
NattapolDedruktip <dedruktip.n@gmail.com>
2024-12-18 11:07:28 +0700

    swiper

--- components/home/LandmarkContainer.tsx
+++ components/home/LandmarkContainer.tsx
@@ -1,6 +1,7 @@
 import { fetchLandmarks } from "@/actions/actions"
 import LandmarkList from "./LandmarkList"
 import { LandmarkCardProps } from "@/utils/types"
+import Hero from "@/app/hero/Hero"
 
 
 const LandmarkContainer = async () => {
@@ -9,6 +10,7 @@ const LandmarkContainer = async () => {
     console.log('landmarks', landmarks)
     return (
         <div>
+            <Hero landmarks={landmarks} />
             <LandmarkList landmarks={landmarks} />
         </div>
     )
